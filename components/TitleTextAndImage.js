80ce487ce3fdcbbb56ddc0d247c794b871546999
anneelv <anneelvira.lauw@gmail.com>
2025-08-05 16:10:06 +1000

    Mock style for task page's title and subtitle header

---
+++ components/TitleTextAndImage.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function TaskPageHeader() {
+  return (
+    <View style={{ padding: 50, paddingTop: 80 }}>
+      <Text style={{ fontSize: 40 }}>Hello</Text>
+      <Text style={{ fontsize: 30 }}>Let's be productive!</Text>
+    </View>
+  );
+}
