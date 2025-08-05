2ffbea6f35176af95ce9d3847e3cf3dccf837e04
anneelv <anneelvira.lauw@gmail.com>
2025-08-05 20:49:09 +1000

    Revamp task page heading position

--- components/TitleTextAndImage.js
+++ components/TitleTextAndImage.js
@@ -3,9 +3,9 @@ import { View } from "react-native";
 
 export default function TaskPageHeader() {
   return (
-    <View style={{ flexDirection: "row", padding: 50, paddingTop: 80 }}>
-      <View style={{ flex: 0.3 }}>{/* Insert image */}</View>
-      <View style={{ flex: 0.7 }}>
+    <View style={{ flexDirection: "row", padding: 50, paddingTop: 100 }}>
+      <View style={{ flex: 0.4 }}>{/* Insert image */}</View>
+      <View style={{ flex: 0.6 }}>
         <ThemedText type="title">Hello</ThemedText>
         <ThemedText type="defaultLato">Let's be productive!</ThemedText>
       </View>
